307ee7c5ed32b86a975da07930e135f26c12ca65
m1runas <gilmetdinovaruslana@gmail.com>
2023-10-16 22:15:04 +0500

    Revert "Revert "Добавление/редактирование услуг(6 лаба)""
    This reverts commit 00920c1ca9db403e8f9c0934d176c74a207d5aa5.

--- Gilmetdinova_autoservice-main/GilmetdinovaAutoservice/GilmetdinovaAutoservice/ServicePage.xaml.cs
+++ Gilmetdinova_autoservice-main/GilmetdinovaAutoservice/GilmetdinovaAutoservice/ServicePage.xaml.cs
@@ -20,6 +20,7 @@ namespace GilmetdinovaAutoservice
     /// </summary>
     public partial class ServicePage : Page
     {
+        
         public ServicePage()
         {
             InitializeComponent();
@@ -31,7 +32,7 @@ namespace GilmetdinovaAutoservice
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
@@ -96,6 +97,25 @@ namespace GilmetdinovaAutoservice
                 currentServices = currentServices.OrderBy(p => p.Cost).ToList();
             }
             ServiceListView.ItemsSource= currentServices;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility== Visibility.Visible)
+            {
+                Gilmetdinova_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p =>p.Reload());
+                ServiceListView.ItemsSource = Gilmetdinova_autoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
